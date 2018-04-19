35c60b4a41300651d135721e8427c17e08e439dd
hy <1115278524@qq.com>
2018-04-20 05:14:38 +0800

    update is_like

--- pcc/view.py
+++ pcc/view.py
@@ -1,6 +1,5 @@
 import aiomysql
 
-
 async def list():
     pass
 
@@ -13,5 +12,17 @@ async def count():
     pass
 
 
-async def is_like():
-    pass
+async def is_like(uid, oid):
+    async with aiomysql.create_pool(host='139.199.0.245', port=3306,
+                           user='dear', password='both-win',
+                           db='pcc') as pool:
+        async with pool.get() as conn:
+            async with conn.cursor() as cur:
+                await res = cur.execute('''SELECT uid, oid
+                                  FROM  favour
+                                  WHERE uid=%s AND oid=%s;''',(uid, oid))
+                if res:
+                    return {'status':'yes'}
+                else:
+                    return {'status':'no'}
+
